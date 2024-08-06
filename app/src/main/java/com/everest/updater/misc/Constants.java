protonplus-org/packages_apps_Updater

Utils: Pass our ziptype prop as parameter to server url
- This is to handle and serve responses separately for vanilla and
  gapps builds.

Change-Id: I553f279a821a8be2c8879f92e83d72157287e923

src/org/protonplus/updater/misc/Constants.java
@@ -42,6 +42,7 @@ public final class Constants {
     public static final String PROP_BUILD_VERSION = "ro.proton.version";
     public static final String PROP_DEVICE = "ro.proton.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
+    public static final String PROP_ZIP_TYPE = "ro.proton.ziptype";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "protonplus.updater.allow_downgrading";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";

ATI-dumpster/Updater

app/src/main/java/com/everest/updater/misc/Constants.java
@@ -40,10 +40,14 @@ public final class Constants {
 
     public static final String PROP_AB_DEVICE = "ro.build.ab_update";
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
-    public static final String PROP_VERSION = "ro.everest.version";
+    public static final String PROP_VERSION = "ro.everest.base.version";
+    public static final String PROP_BUILD_VERSION_INCREMENTAL = "ro.build.version.incremental";
     public static final String PROP_DEVICE = "ro.everest.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
+    public static final String PROP_RELEASE_TYPE = "ro.everest.buildtype";
+    public static final String PROP_ZIP_TYPE = "ro.everest.edition";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "everest.updater.allow_downgrading";
+    public static final String PROP_UPDATER_URI = "everest.updater.uri";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";